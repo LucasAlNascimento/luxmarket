e3dcc54d491288e1f63c2bba4793bc3236254ba2
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-07-11 20:04:21 -0300

    Alterando nome da empresa no Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,8 +7,8 @@ function Header(){
     return(
         <header className="header">
             <div className="container">
-                <div className="logomks">MKS</div>
-                <div className="logosistemas">Sistemas</div>
+                <div className="logomks">LUX</div>
+                <div className="logosistemas">Market</div>
             </div>
                 <CartButton />
         </header>
